54485a97b1ae611914954732e0deac550a3fa972
Quentin Kniep <hello@quentinkniep.com>
2020-12-01 10:46:30 +0100

    day01: read from stdin, reuse outer loop of 2 num search

--- 2020/day01/reportRepair.go
+++ 2020/day01/reportRepair.go
@@ -3,36 +3,33 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"strconv"
 )
 
 func main() {
+	// read integers (each on a single line) from stdin
 	var nums []int
-	file, _ := os.Open("./input.txt")
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		i, _ := strconv.Atoi(scanner.Text())
-		nums = append(nums, i)
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+		num, err := strconv.Atoi(scanner.Text())
+		if err == nil {
+			nums = append(nums, num)
+		}
 	}
 
+	// search for 2 or 3 nums which sum to 2020
 	for a := 0; a < len(nums); a++ {
 		for b := a+1; b < len(nums); b++ {
+			// check for a 2 number match
 			if nums[a] + nums[b] == 2020 {
 				fmt.Print("2 Entries: ")
 				fmt.Printf("%d * %d = %d\n", nums[a], nums[b], nums[a] * nums[b])
 			}
-		}
-	}
 
-	for a := 0; a < len(nums); a++ {
-		for b := a+1; b < len(nums); b++ {
+			// search for a 3rd number which makes this sum to 2020
 			for c := b+1; c < len(nums); c++ {
+				// check for a 3 number match
 				if nums[a] + nums[b] + nums[c] == 2020 {
 					fmt.Print("3 Entries: ")
 					product := nums[a] * nums[b] * nums[c]
