37ce07227d3b56b93eb7171fd8eab5b308cb77f1
Quentin Kniep <hello@quentinkniep.com>
2020-12-07 14:13:19 +0100

    🎨 cleanup, formatting, comments

--- 2020/day07/handyHaversacks.go
+++ 2020/day07/handyHaversacks.go
@@ -9,59 +9,58 @@ import (
 )
 
 type bags struct {
-	num int;
-	color string;
+	num   int
+	color string
 }
 
 func main() {
-	var rules = make(map[string][]string, 0)
+	var rulesOuter = make(map[string][]string, 0)
 	var rulesCount = make(map[string][]bags, 0)
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		var words = strings.Fields(scanner.Text())
-		bagColor := fmt.Sprintf("%s %s", words[0], words[1])
-		for i := 5; i + 2 < len(words); i += 4 {
-			color := fmt.Sprintf("%s %s", words[i], words[i+1])
-			rules[color] = append(rules[color], bagColor)
+		outColor := words[0] + " " + words[1]
+		// parse all inner bags for this one outer bag, add to rules
+		for i := 5; i+2 < len(words); i += 4 {
+			inColor := words[i] + " " + words[i+1]
 			num, _ := strconv.Atoi(words[i-1])
-			rulesCount[bagColor] = append(rulesCount[bagColor], bags{num, color})
+			rulesOuter[inColor] = append(rulesOuter[inColor], outColor)
+			rulesCount[outColor] = append(rulesCount[outColor], bags{num, inColor})
 		}
 	}
 
-	var numPossibleOuter = len(possibleOuter(rules, "shiny gold"))
-	var numTotalInner = totalInner(rulesCount, "shiny gold")
-
-	fmt.Printf("Possible outer colors: %d\n", numPossibleOuter)
-	fmt.Printf("Everyone answered yes: %d\n", numTotalInner)
+	fmt.Printf("Possible outer bag colors: %d\n", possibleOuter(rulesOuter, "shiny gold"))
+	fmt.Printf("Total number of inner bags: %d\n", totalInner(rulesCount, "shiny gold"))
 }
 
-func possibleOuter(rules map[string][]string, color string) []string {
+// Gives the number of possible outer bags which contain a `color` bag somewhere inside.
+func possibleOuter(rules map[string][]string, color string) int {
 	added := make(map[string]bool, 0)
-	outer, dirty := make([]string, 0), rules[color]
+	dirty := rules[color]
+	count := 0
 
 	for _, c := range rules[color] {
 		added[c] = true
 	}
 
-	for len(dirty) > 0 {
-		d := dirty[0]
-		outer = append(outer, d)
+	for d := dirty[0]; len(dirty) > 0; d, dirty = dirty[0], dirty[1:] {
 		for _, c := range rules[d] {
 			if !added[c] {
 				dirty = append(dirty, c)
 				added[c] = true
 			}
 		}
-		dirty = dirty[1:]
+		count++
 	}
-	return outer
+	return count
 }
 
+// Gives the total number of bags which are needed inside one `color` bag, based on `rules`.
 func totalInner(rules map[string][]bags, color string) int {
 	sum := 0
 	for _, b := range rules[color] {
-		sum += b.num + b.num * totalInner(rules, b.color)
+		sum += b.num + b.num*totalInner(rules, b.color)
 	}
 	return sum
 }
