cb4cea02e9088cb54abb27169511e9eb1f5bac32
Quentin Kniep <hello@quentinkniep.com>
2020-12-09 18:12:24 +0100

    💡 day9: add comments

--- 2020/day09/encodingError.go
+++ 2020/day09/encodingError.go
@@ -11,40 +11,51 @@ import (
 func main() {
 	var nums = make([]int, 0)
 
+	// read all numbers into an array
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		num, _ := strconv.Atoi(scanner.Text())
 		nums = append(nums, num)
 	}
 
-	fmt.Printf("Acc value: %d\n", findNonSum(nums))
-	fmt.Printf("Fixed Acc value: %d\n", findEncWeakness(nums, 90433990))
+	nonSum := findNonSum(nums, 25)
+	encWeakness := findEncWeakness(nums, nonSum)
+
+	fmt.Printf("Acc value: %d\n", nonSum)
+	fmt.Printf("Fixed Acc value: %d\n", encWeakness)
 }
 
-func findNonSum(nums []int) int {
-	for i := 25; i < len(nums); i++ {
+// Finds the first entry in `nums` which is not the sum of any two of the `slide` entries
+// immediately before it, ignoring the `slide` first entries.
+// Runs in: O(n*(slide^2)), where n=len(nums).
+func findNonSum(nums []int, slide int) int {
+	for i := slide; i < len(nums); i++ {
 		sumOfTwo := false
-		for a := 1; a < 25; a++ {
+		for a := 1; a < slide; a++ {
 			for b := 0; b < a; b++ {
-				if nums[i+a] + nums[i+b] == nums[i+25] {
+				if nums[i+a] + nums[i+b] == nums[i+slide] {
 					sumOfTwo = true
 				}
 			}
 		}
 		if !sumOfTwo {
-			return nums[i+25]
+			return nums[i+slide]
 		}
 	}
 	return -1
 }
 
+// Finds the first contiguous range of entries in `nums` which sum to `sum`, returning the sum of
+// the smallest and largest number in that range.
+// Runs in: O(n^2), where n=len(nums).
 func findEncWeakness(nums []int, sum int) int {
 	for i := 0; i < len(nums); i++ {
 		runningSum := nums[i]
 		for j := i+1; j < len(nums); j++ {
 			runningSum += nums[j]
 			if runningSum == sum {
-				sorted := nums[i:j+1]
+				sorted := make([]int, j-i+1)
+				copy(sorted, nums[i:j+1])
 				sort.Ints(sorted)
 				return sorted[0]+sorted[len(sorted)-1]
 			} else if runningSum > sum {
