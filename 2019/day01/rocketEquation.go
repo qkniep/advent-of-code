53b0ea065000992bcaa7f97c4a02493c65e49031
Quentin Kniep <hello@quentinkniep.com>
2020-12-12 22:02:19 +0100

    ✨ solve day 1 of 2019 AoC

---
+++ 2019/day01/rocketEquation.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var fuelSum, recursiveFuelSum = 0, 0
+
+	// read all mass numbers and calculate the fuel requirements, keeping track of the sums
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		mass, _ := strconv.Atoi(scanner.Text())
+		fuelSum += fuelRequirement(mass)
+		recursiveFuelSum += recursiveFuelRequirement(mass)
+	}
+
+	fmt.Printf("Fuel requirement for mass: %d\n", fuelSum)
+	fmt.Printf("Recursive fuel requirements: %d\n", recursiveFuelSum)
+}
+
+func fuelRequirement(mass int) int {
+	return mass / 3 - 2
+}
+
+// Calculates the amount of fuel needed for the base mass and all the fuel.
+func recursiveFuelRequirement(mass int) int {
+	fuel := fuelRequirement(mass)
+	if fuel <= 0 {
+		return 0
+	}
+	return fuel + recursiveFuelRequirement(fuel)
+}
