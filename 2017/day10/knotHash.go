fbe7ec5dc5f6b981b7fb13bb035ddeb500d6b773
Quentin Kniep <hello@quentinkniep.com>
2020-12-13 01:43:18 +0100

    ✨ solve day 10 part 2 of 2017 AoC

--- 2017/day10/knotHash.go
+++ 2017/day10/knotHash.go
@@ -1,15 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	list := createList(256)
-	lengths := []int{227, 169, 3, 166, 246, 201, 0, 47, 1, 255, 2, 254, 96, 3, 97, 144}
-	prepareList(list, lengths)
-	fmt.Printf("Product of first two values: %v\n", list[0]*list[1])
-	//fmt.Printf("Garbaged Characters: %v\n", garbaged)
+	var intList, byteList = createList(256), createList(256)
+	var intLengths, byteLengths = make([]int, 0), make([]int, 0)
+
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	// interpret input as comma seperated numbers
+	for _, n := range strings.Split(input, ",") {
+		i, _ := strconv.Atoi(n)
+		intLengths = append(intLengths, i)
+	}
+	// interpret input as ASCII byte values and append suffix
+	for _, b := range strings.TrimSuffix(input, "\n") {
+		byteLengths = append(byteLengths, int(byte(b)))
+	}
+	byteLengths = append(byteLengths, 17, 31, 73, 47, 23)
+
+	hash(intList, intLengths, 1)
+	hash(byteList, byteLengths, 64)
+	denseHash := densify(byteList)
+
+	fmt.Printf("Product of first two values: %v\n", intList[0]*intList[1])
+	fmt.Printf("Dense hash hex string: %s\n", denseHash)
 }
 
+// Initialize a new list of given size where each element is equal to its index.
 func createList(size int) (list []int) {
 	list = make([]int, size)
 	for i := 0; i < size; i++ {
@@ -18,18 +42,34 @@ func createList(size int) (list []int) {
 	return
 }
 
-func prepareList(list []int, lengths []int) {
+// Perform given number of iterations of the knot-tying hash algorithm, based on lengths.
+// Results are written into the list slice.
+func hash(list []int, lengths []int, iterations int) {
 	currentPos := 0
 	skipSize := 0
-	for _, l := range lengths {
-		a, b := currentPos, currentPos+l-1
-		for a < b {
-			tmp := list[a%len(list)]
-			list[a%len(list)] = list[b%len(list)]
-			list[b%len(list)] = tmp
-			a, b = a+1, b-1
+	for i := 0; i < iterations; i++ {
+		for _, l := range lengths {
+			a, b := currentPos, currentPos+l-1
+			for a < b {
+				tmp := list[a%len(list)]
+				list[a%len(list)] = list[b%len(list)]
+				list[b%len(list)] = tmp
+				a, b = a+1, b-1
+			}
+			currentPos += l + skipSize
+			skipSize++
 		}
-		currentPos += l + skipSize
-		skipSize += 1
 	}
+}
+
+// Generates the dense hash representation as hex string.
+func densify(sparse []int) (dense string) {
+	for block := 0; block < 16; block++ {
+		char := 0
+		for i := 0; i < 16; i++ {
+			char ^= sparse[16*block+i]
+		}
+		dense = fmt.Sprintf("%s%x", dense, char)
+	}
+	return
 }
