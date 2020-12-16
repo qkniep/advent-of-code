1e73ddceaf50c8803f476d0742023f6001735b20
Quentin Kniep <hello@quentinkniep.com>
2020-12-16 19:35:56 +0100

    🎨 day16: clean up a little bit

--- 2020/day16/ticketTranslation.go
+++ 2020/day16/ticketTranslation.go
@@ -13,6 +13,7 @@ func main() {
 	var validTickets, myTicket = make([][]int, 0), make([]int, 0)
 	var min, max int
 
+	// read entry valid intervals
 	scanner := bufio.NewScanner(os.Stdin)
 	for entry := 0; scanner.Scan(); entry++ {
 		if len(scanner.Text()) == 0 {
@@ -30,17 +31,16 @@ func main() {
 		}
 	}
 
+	// read own ticket values
 	scanner.Scan() // ignore "your ticket" line
-	for scanner.Scan() {
-		if len(scanner.Text()) == 0 {
-			break
-		}
-		for _, numStr := range strings.Split(scanner.Text(), ",") {
-			num, _ := strconv.Atoi(numStr)
-			myTicket = append(myTicket, num)
-		}
+	scanner.Scan()
+	for _, numStr := range strings.Split(scanner.Text(), ",") {
+		num, _ := strconv.Atoi(numStr)
+		myTicket = append(myTicket, num)
 	}
+	scanner.Scan() // ignore empty line
 
+	// read nearby tickets
 	scanner.Scan() // ignore "nearby tickets" line
 	errorRate := 0
 	for scanner.Scan() {
@@ -73,56 +73,34 @@ func main() {
 		for pos, value := range ticket {
 			for entry := range validNumsForEntry {
 				if !validNumsForEntry[entry][value] {
-					fmt.Printf("Offense: %v not allowed for entry %v, found at pos %v\n", value, entry, pos)
 					possible[entry][pos] = false
 				}
 			}
 		}
 	}
 
-	fmt.Printf("%v\n", possible)
-
 	dirty := true
 	for dirty {
 		dirty = false
 		for pos1 := range validNumsForEntry {
-			countF, countB := 0, 0
-			valueF, valueB := 0, 0
-			//countF := 0
-			//valueF := 0
+			count := 0
+			value := 0
 			for pos2 := range validNumsForEntry {
 				if possible[pos1][pos2] { // for entry `pos1` position `pos2` on the ticket is possible
-					countF++
-					valueF = pos2
-				}
-				if possible[pos2][pos1] { // for entry `pos2` position `pos1` on the ticket is possible
-					countB++
-					valueB = pos2
+					count++
+					value = pos2
 				}
 			}
-			// forward
-			if countF == 1 { // for entry `pos1` only one position `valueF` is possible
+			if count == 1 { // for entry `pos1` only one position `valueF` is possible
 				for pos2 := range validNumsForEntry {
 					if pos2 != pos1 {
-						possible[pos2][valueF] = false
-					}
-				}
-			} else {
-				dirty = true
-			}
-			// backward
-			if countB == 1 { // for position `pos1` only one entry `valueB` is possible
-				for pos2 := range validNumsForEntry {
-					if pos2 != pos1 {
-						possible[valueB][pos2] = false
+						possible[pos2][value] = false
 					}
 				}
 			} else {
 				dirty = true
 			}
 		}
-
-	fmt.Printf("%v\n", possible)
 	}
 
 	correctPositions := make([]int, len(validNumsForEntry))
