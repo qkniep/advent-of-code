b1841ba913edc334f1812402168c50e27a56fbd3
Quentin Kniep <hello@quentinkniep.com>
2020-12-24 02:01:29 +0100

    🚧 day 21: first attempt at naive solution for operator precedence

--- 2020/day18/operationOrder.go
+++ 2020/day18/operationOrder.go
@@ -9,16 +9,18 @@ import (
 )
 
 func main() {
-	var sum = 0
+	var sum1, sum2 = 0, 0
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		expr := strings.Replace(scanner.Text(), "(", "( ", -1)
 		expr = strings.Replace(expr, ")", " )", -1)
-		sum += evaluateExpression(strings.Fields(expr))
+		sum1 += evaluateExpression(strings.Fields(expr))
+		sum2 += evaluateExpression2(strings.Fields(expr))
 	}
 
-	fmt.Println(sum)
+	fmt.Println(sum1)
+	fmt.Println(sum2)
 }
 
 func evaluateExpression(tokens []string) int {
@@ -34,6 +36,23 @@ func evaluateExpression(tokens []string) int {
 			result *= n
 		}
 	}
+	return result
+}
+
+func evaluateExpression2(tokens []string) int {
+	i := 0
+	result := evaluateParensOrAdd(tokens, &i)
+	for i < len(tokens) {
+		op := tokens[i]
+		i++
+		n := evaluateParensOrAdd(tokens, &i)
+		if op == "+" {
+			result += n
+		} else if op == "*" {
+			result *= n
+		}
+	}
+	fmt.Printf("%v -> %v\n", tokens, result)
 	return result
 }
 
@@ -53,6 +72,31 @@ func evaluateParens(tokens []string, pos *int) int {
 			parens--
 			if parens == 0 {
 				return evaluateExpression(tokens[start+1:*pos-1])
+			}
+		}
+	}
+}
+
+func evaluateParensOrAdd(tokens []string, pos *int) int {
+	start := *pos
+	parens := 0
+	if tokens[start] != "(" {
+		n, _ := strconv.Atoi(tokens[*pos])
+		if start+1 < len(tokens) && tokens[start+1] == "+" {
+			*pos += 2
+			return n + evaluateParensOrAdd(tokens, pos)
+		}
+		*pos++
+		return n
+	}
+	for {
+		*pos++
+		if tokens[*pos-1] == "(" {
+			parens++
+		} else if tokens[*pos-1] == ")" {
+			parens--
+			if parens == 0 {
+				return evaluateExpression2(tokens[start+1:*pos-1])
 			}
 		}
 	}
