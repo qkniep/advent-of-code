a5dfd4b301b13052c981614f95cda9cb31e444e2
Quentin Kniep <hello@quentinkniep.com>
2021-01-01 19:25:43 +0100

    ✨ day 18 part 2

--- 2020/day18/operationOrder.go
+++ 2020/day18/operationOrder.go
@@ -8,6 +8,21 @@ import (
 	"strings"
 )
 
+type operator struct {
+	prec   int
+	rAssoc bool
+}
+
+var ops1 = map[string]operator {
+	"*": {1, false},
+	"+": {1, false},
+}
+
+var ops2 = map[string]operator {
+	"*": {1, false},
+	"+": {2, false},
+}
+
 func main() {
 	var sum1, sum2 = 0, 0
 
@@ -15,89 +30,75 @@ func main() {
 	for scanner.Scan() {
 		expr := strings.Replace(scanner.Text(), "(", "( ", -1)
 		expr = strings.Replace(expr, ")", " )", -1)
-		sum1 += evaluateExpression(strings.Fields(expr))
-		sum2 += evaluateExpression2(strings.Fields(expr))
+		sum1 += evaluateRPN(shuntingYard(strings.Fields(expr), ops1))
+		sum2 += evaluateRPN(shuntingYard(strings.Fields(expr), ops2))
 	}
 
 	fmt.Println(sum1)
 	fmt.Println(sum2)
 }
 
-func evaluateExpression(tokens []string) int {
-	i := 0
-	result := evaluateParens(tokens, &i)
-	for i < len(tokens) {
-		op := tokens[i]
-		i++
-		n := evaluateParens(tokens, &i)
-		if op == "+" {
-			result += n
-		} else if op == "*" {
-			result *= n
-		}
-	}
-	return result
-}
-
-func evaluateExpression2(tokens []string) int {
-	i := 0
-	result := evaluateParensOrAdd(tokens, &i)
-	for i < len(tokens) {
-		op := tokens[i]
-		i++
-		n := evaluateParensOrAdd(tokens, &i)
-		if op == "+" {
-			result += n
-		} else if op == "*" {
-			result *= n
-		}
-	}
-	fmt.Printf("%v -> %v\n", tokens, result)
-	return result
-}
-
-func evaluateParens(tokens []string, pos *int) int {
-	start := *pos
-	parens := 0
-	if tokens[start] != "(" {
-		n, _ := strconv.Atoi(tokens[*pos])
-		*pos++
-		return n
-	}
-	for {
-		*pos++
-		if tokens[*pos-1] == "(" {
-			parens++
-		} else if tokens[*pos-1] == ")" {
-			parens--
-			if parens == 0 {
-				return evaluateExpression(tokens[start+1:*pos-1])
+// Turn infix notation into postfix (reverse polish) notation.
+// Source: https://rosettacode.org/wiki/Parsing/Shunting-yard_algorithm#Go
+func shuntingYard(tokens []string, ops map[string]operator) (output []string) {
+	var stack []string
+	for _, t := range tokens {
+		switch t {
+		case "(":
+			stack = append(stack, t)
+		case ")":
+			var op string
+			for {
+				// pop item ("(" or operator) from stack
+				op, stack = stack[len(stack)-1], stack[:len(stack)-1]
+				if op == "(" {
+					break // discard "("
+				}
+				output = append(output,  op)
+			}
+		default:
+			if o1, isOp := ops[t]; isOp { // token is an operator
+				for len(stack) > 0 {
+					// consider top item on stack
+					op := stack[len(stack)-1]
+					if o2, isOp := ops[op]; !isOp || o1.prec > o2.prec ||
+						o1.prec == o2.prec && o1.rAssoc {
+						break
+					}
+					// top item is an operator that needs to come off
+					stack = stack[:len(stack)-1] // pop it
+					output = append(output, op)
+				}
+				// push operator (the new one) to stack
+				stack = append(stack, t)
+			} else { // token is an operand
+				output = append(output, t)
 			}
 		}
 	}
+
+	for i := len(stack)-1; i >= 0; i-- {
+		output = append(output, stack[i])
+	}
+	return
 }
 
-func evaluateParensOrAdd(tokens []string, pos *int) int {
-	start := *pos
-	parens := 0
-	if tokens[start] != "(" {
-		n, _ := strconv.Atoi(tokens[*pos])
-		if start+1 < len(tokens) && tokens[start+1] == "+" {
-			*pos += 2
-			return n + evaluateParensOrAdd(tokens, pos)
-		}
-		*pos++
-		return n
-	}
-	for {
-		*pos++
-		if tokens[*pos-1] == "(" {
-			parens++
-		} else if tokens[*pos-1] == ")" {
-			parens--
-			if parens == 0 {
-				return evaluateExpression2(tokens[start+1:*pos-1])
-			}
+// Evaluate a calculation
+func evaluateRPN(tokens []string) int {
+	var stack []int
+	for _, t := range tokens {
+		l := len(stack)
+		switch t {
+		case "+":
+			stack[l-2] += stack[l-1]
+			stack = stack[:l-1]
+		case "*":
+			stack[l-2] *= stack[l-1]
+			stack = stack[:l-1]
+		default:
+			n, _ := strconv.Atoi(t)
+			stack = append(stack, n)
 		}
 	}
+	return stack[0]
 }
