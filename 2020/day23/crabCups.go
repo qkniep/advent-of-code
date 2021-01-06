411b10a852e536bb7a9dd1f3ecb37346f138ff02
Quentin Kniep <hello@quentinkniep.com>
2021-01-06 10:46:03 +0100

    ✨ day 23 part 2

--- 2020/day23/crabCups.go
+++ 2020/day23/crabCups.go
@@ -7,56 +7,75 @@ import (
 )
 
 func main() {
-	var cups []int
+	var nineCups, millionCups []int
 
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-	cups = make([]int, len(scanner.Text()))
-	for i, cup := range scanner.Bytes() {
-		cups[i] = int(cup - '0')
+	nineCups = make([]int, len(scanner.Bytes())+1)
+	previous, first := int(scanner.Bytes()[0] - '0'), int(scanner.Bytes()[0] - '0')
+	for _, cup := range scanner.Bytes()[1:] {
+		nineCups[previous] = int(cup - '0')
+		previous = int(cup - '0')
 	}
+	nineCups[previous] = first
 
-	// perform 100 game rounds
-	for round := 0; round < 100; round++ {
-		newCups := make([]int, len(cups))
-		pickedUp := cups[1:4]
-		// determine destination cup label
-		destinationCup := cups[0] - 1
-		if destinationCup == 0 {
-			destinationCup = 9
-		}
-		for destinationCup == pickedUp[0] || destinationCup == pickedUp[1] || destinationCup == pickedUp[2] {
-			destinationCup--
-			if destinationCup == 0 {
-				destinationCup = 9
-			}
-		}
-		// build the new cup arrangement
-		newCups[0] = cups[4]
-		for i, added := 1, 0; i < len(cups); i++ {
-			modI := (4+i)%len(cups)
-			if modI >= 1 && modI <= 3 {
-				continue
-			} else if cups[(3+i)%len(cups)] == destinationCup {
-				copy(newCups[1+added:4+added], cups[1:4])
-				added += 3
-			}
-			newCups[1+added] = cups[modI]
-			added++
-		}
-		fmt.Printf("%v\n", newCups)
-		copy(cups, newCups)
+	millionCups = make([]int, 1_000_000+1)
+	copy(millionCups, nineCups)
+	millionCups[previous] = 10
+	for i := 10; i <= 1_000_000; i++ {
+		millionCups[previous] = i
+		previous = i
 	}
+	millionCups[previous] = first
 
-	// determine final order
-	var onePos int
-	for pos, cup := range cups {
-		if cup == 1 {
-			onePos = pos
-		}
-	}
-	for i := 1; i < len(cups); i++ {
-		fmt.Printf("%v", cups[(onePos+i)%len(cups)])
+	// perform the game rounds
+	runCrabGame(nineCups, 100, first)
+	runCrabGame(millionCups, 10_000_000, first)
+
+	// determine final order (part 1)
+	for i := nineCups[1]; i != 1; i = nineCups[i] {
+		fmt.Printf("%v", i)
 	}
 	fmt.Printf("\n")
+
+	// find the two cups immediately after 1 and multiply their IDs (part 2)
+	for i := millionCups[1]; ; i = millionCups[i] {
+		if i == 1 {
+			c1 := millionCups[i]
+			c2 := millionCups[c1]
+			fmt.Println(c1 * c2)
+			break
+		}
+	}
+}
+
+func runCrabGame(cups []int, rounds int, startCup int) {
+	var current = startCup
+	var pickedUp = make([]int, 3)
+
+	for round := 0; round < rounds; round++ {
+		// pick up next 3 cups
+		pickedUp[0] = cups[current]
+		pickedUp[1] = cups[pickedUp[0]]
+		pickedUp[2] = cups[pickedUp[1]]
+
+		// determine destination cup label
+		destination := current - 1
+		if destination == 0 {
+			destination = len(cups)-1
+		}
+		for destination == pickedUp[0] || destination == pickedUp[1] || destination == pickedUp[2] {
+			destination--
+			if destination == 0 {
+				destination = len(cups)-1
+			}
+		}
+
+		// build the new cup arrangement
+		tmp := cups[pickedUp[2]]
+		cups[pickedUp[2]] = cups[destination]
+		cups[destination] = cups[current]
+		cups[current] = tmp
+		current = cups[current]
+	}
 }
