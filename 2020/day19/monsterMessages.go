3f30e70a51cb6c901e5d850fd4c98bd37e3be8c4
Quentin Kniep <hello@quentinkniep.com>
2021-01-08 13:18:07 +0100

    ✨ day 19 part 2

--- 2020/day19/monsterMessages.go
+++ 2020/day19/monsterMessages.go
@@ -10,7 +10,6 @@ import (
 
 func main() {
 	var rules = make(map[int]string, 128)
-	var possible = make(map[string]bool, 0)
 	var matches = 0
 
 	scanner := bufio.NewScanner(os.Stdin)
@@ -23,65 +22,68 @@ func main() {
 		rules[n] = scanner.Text()[len(fields[0]):]
 	}
 
-	// generate possible values
-	possible = possibleValues(rules, 0)
-
+	var inputs []string
 	for scanner.Scan() {
-		//if (ruleMatches(scanner.Text(), rules, 0)) {
-		if possible[scanner.Text()] {
+		inputs = append(inputs, scanner.Text())
+	}
+
+	for _, s := range inputs {
+		if ruleMatchesFullStr(s, rules, 0) {
 			matches++
 		}
 	}
-
 	fmt.Printf("Matches: %v\n", matches)
+
+	rules[8] = "42 | 42 8"
+	rules[11] = "42 31 | 42 11 31"
+
+	matches = 0
+	for _, s := range inputs {
+		if ruleMatchesFullStr(s, rules, 0) {
+			matches++
+		}
+	}
+	fmt.Printf("Matches with recursive rules: %v\n", matches)
 }
 
-func possibleValues(rules map[int]string, rule int) map[string]bool {
-	fmt.Printf("Parse rule #%v.\n", rule)
-	var possible = make(map[string]bool, 0)
+// Returns true iff the full string `s` exactly matches the given rule.
+func ruleMatchesFullStr(s string, rules map[int]string, ruleToMatch int) bool {
+	var finalIndices = matchRule(s, rules, ruleToMatch, 0)
+	for _, index := range finalIndices {
+		if index == len(s) {
+			return true
+		}
+	}
+	return false
+}
+
+// Checks whether a given rule could match the string `s` starting at index `from`.
+// Returns a list of all possible indices where we could end up after matching the rule,
+// this is list is empty iff it's impossible to match the rule at that index.
+func matchRule(s string, rules map[int]string, rule int, from int) []int {
+	if from >= len(s) {
+		return []int{}
+	}
+	// check base rules (match a single character)
 	if rules[rule] == " \"a\"" || rules[rule] == " \"b\"" {
-		possible[rules[rule][2:3]] = true
-	} else if strings.Contains(rules[rule], "|") {
-		for _, subRule := range strings.Split(rules[rule], "|") {
-			for s := range concatPossibleValues(subRule, rules, rule) {
-				possible[s] = true
+		if s[from] == rules[rule][2] {
+			return []int{from+1}
+		}
+		return []int{}
+	}
+	// check all other rules
+	var finalIndices []int
+	for _, subRule := range strings.Split(rules[rule], "|") {
+		oldIndices := []int{from}
+		for _, ruleStr := range strings.Fields(subRule) {
+			ruleInt, _ := strconv.Atoi(ruleStr)
+			newIndices := []int{}
+			for _, index := range oldIndices {
+				newIndices = append(newIndices, matchRule(s, rules, ruleInt, index)...)
 			}
+			oldIndices = newIndices
 		}
-	} else {
-		for s := range concatPossibleValues(rules[rule], rules, rule) {
-			possible[s] = true
-		}
+		finalIndices = append(finalIndices, oldIndices...)
 	}
-	return possible
-}
-
-func concatPossibleValues(s string, rules map[int]string, rule int) map[string]bool {
-	var subRules = strings.Fields(s)
-	var possible = make([]map[string]bool, len(subRules))
-	for i := range subRules {
-		possible[i] = make(map[string]bool, 0)
-	}
-	for i, r := range subRules {
-		n, _ := strconv.Atoi(r)
-		for s := range possibleValues(rules, n) {
-			possible[i][s] = true
-		}
-	}
-	if len(subRules) == 1 {
-		return possible[0]
-	}
-	if len(subRules) != 2 {
-		panic("GRRRR!")
-	}
-	var result = make(map[string]bool, 0)
-	for s1 := range possible[0] {
-		for s2 := range possible[1] {
-			result[s1+s2] = true
-		}
-	}
-	return result
-}
-
-func ruleMatches(s string, rules map[int]string, ruleToMatch int) bool {
-	return true
+	return finalIndices
 }
