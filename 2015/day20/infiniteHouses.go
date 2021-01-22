2fe6888bdfcc6d5e7341b7b6879a5baf1d65852b
Quentin Kniep <hello@quentinkniep.com>
2021-01-22 13:00:35 +0100

    ✨ 2015 day 20 part 2

--- 2015/day20/infiniteHouses.go
+++ 2015/day20/infiniteHouses.go
@@ -1,52 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	var presents, house int
-
+	var presents, house1, house2 int64
 	fmt.Scanf("%d", &presents)
 
-	house = findSmallestWithDivisorSum(presents / 10)
+	house1 = findSmallestWithDivisorSum(presents / 10, math.MaxInt64)
+	house2 = findSmallestWithDivisorSum(presents / 11, 50)
 
-	fmt.Printf("First house to receive %d presents: %d\n", presents, house)
-	//fmt.Println("Total brightness:", totalBrightness)
+	fmt.Printf("First house (%d presents, 10 per Elf, no max): %d\n", presents, house1)
+	fmt.Printf("First house (%d presents, 11 per Elf, 50 houses max): %d\n", presents, house2)
 }
 
 // Returns the smallest number where all its divisors added together produce the given sum.
-func findSmallestWithDivisorSum(divisorSum int) int {
-	//var primes []int
-	for i := 2; ; i++ {
-		if i % 100000 == 0 {
-			fmt.Println(i)
-		}
-		// find prime factors
-		/*for _, p := range primes {
-			if i % p == 0 {
-				for a := p; a <= i; a += p {
-				sum += p
-				if sum >= factorSum {
-					break
-				}
-			}
-		}*/
+// Also ignores all divisors for which the quotient is larger than max.
+// Simply set max to math.MaxInt64 to not use this feature.
+func findSmallestWithDivisorSum(divisorSum int64, max int64) int64 {
+	for i := int64(2); ; i++ {
 		// calculate sum of all factors
-		var sum = i+1 // 1 and i itself are divisors of every i
-		for j := 2; j*j <= i; j++ {
+		var sum int64
+		for j := int64(1); j*j <= i; j++ {
 			if i % j == 0 {
-				sum += j
-				if j*j < i {
+				if i / j <= max {
+					sum += j
+				}
+				if j*j < i && j <= max {
 					sum += i/j
 				}
 			}
 		}
-		if i < 20 {
-			fmt.Printf("Divisor sum of %v: %v\n", i, sum)
-		}
 		if sum >= divisorSum {
 			return i
-		}/* else if sum == i+1 {
-			primes = append(primes, i)
-		}*/
+		}
 	}
 }
