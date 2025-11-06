e1cae29de7b39b097a0b91584805ab9d10bb9136
Quentin Kniep <hello@quentinkniep.com>
2025-11-06 01:58:49 +0100

    fix go build

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 # Usage: ./scripts/run.sh rust 2015 day01 part1
 
+lang=$1 year=$2 day=$3 part=$4
+
 case $lang in
-  go)      (cd go/$project && go run ./$(echo "$@" | tr ' ' '/')) ;;
+  go)      (cd go && go run ./$year/$day/*.go < ../data/$year/inputs/$day.txt) ;;
   ocaml)   (cd ocaml && dune exec ${project}/$1.exe) ;;
-  rust)    (cd rust/$project && cargo run -- "$@") ;;
+  rust)    (cd rust && cargo run -- "$@") ;;
   zig)     (cd zig && zig build run-${project}-$1) ;;
   *) echo "Unknown language: $lang" ;;
 esac
