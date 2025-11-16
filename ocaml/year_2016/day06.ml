abb8aa2bc05e13676fe3b3afe53cedc1e5f3503f
Quentin Kniep <hello@quentinkniep.com>
2025-11-16 19:24:13 +0100

    `ocamlformat`

--- ocaml/year_2016/day06.ml
+++ ocaml/year_2016/day06.ml
@@ -7,37 +7,35 @@ let most_common_letters input =
   let counts = Array.init (26 * len) ~f:(fun _ -> 0) in
   let most_common = Bytes.init len ~f:(fun _ -> 'a') in
   List.iter input ~f:(fun word ->
-    String.to_sequence word
-    |> Sequence.iteri ~f:(fun i c ->
-        let mc_char = Bytes.get most_common i in
-        let mc_idx = (26 * i) + (Char.to_int mc_char - Char.to_int 'a') in
-        let mc_count = Array.get counts mc_idx in
-        let c_idx = (26 * i) + (Char.to_int c - Char.to_int 'a') in
-        let c_count = Array.get counts c_idx in
-        Array.set counts c_idx (c_count + 1);
-        if c_count + 1 > mc_count then Bytes.set most_common i c));
+      String.to_sequence word
+      |> Sequence.iteri ~f:(fun i c ->
+             let mc_char = Bytes.get most_common i in
+             let mc_idx = (26 * i) + (Char.to_int mc_char - Char.to_int 'a') in
+             let mc_count = Array.get counts mc_idx in
+             let c_idx = (26 * i) + (Char.to_int c - Char.to_int 'a') in
+             let c_count = Array.get counts c_idx in
+             Array.set counts c_idx (c_count + 1);
+             if c_count + 1 > mc_count then Bytes.set most_common i c));
   most_common
 
 let count_letters input =
   let len = String.length (List.hd_exn input) in
   let counts = Array.init (26 * len) ~f:(fun _ -> 0) in
   List.iter input ~f:(fun word ->
-    String.to_sequence word
-    |> Sequence.iteri ~f:(fun i c ->
-        let c_idx = (26 * i) + (Char.to_int c - Char.to_int 'a') in
-        let c_count = Array.get counts c_idx in
-        Array.set counts c_idx (c_count + 1)));
+      String.to_sequence word
+      |> Sequence.iteri ~f:(fun i c ->
+             let c_idx = (26 * i) + (Char.to_int c - Char.to_int 'a') in
+             let c_count = Array.get counts c_idx in
+             Array.set counts c_idx (c_count + 1)));
   counts
 
 let least_common_letters input =
-  count_letters input
-  |> Array.chunks_of ~length:26
+  count_letters input |> Array.chunks_of ~length:26
   |> Array.map ~f:(fun arr ->
-      Array.to_sequence arr
-      |> Sequence.mapi ~f:(fun i x -> (i, x))
-      |> Sequence.min_elt ~compare:(fun (_, x) (_, y) -> Int.compare x y)
-      |> Option.value_exn
-      |> fst)
+         Array.to_sequence arr
+         |> Sequence.mapi ~f:(fun i x -> (i, x))
+         |> Sequence.min_elt ~compare:(fun (_, x) (_, y) -> Int.compare x y)
+         |> Option.value_exn |> fst)
   |> Array.map ~f:(fun i -> Char.of_int_exn (Char.to_int 'a' + i))
 
 module Day06 : DAY = struct
@@ -49,9 +47,7 @@ module Day06 : DAY = struct
   let parse_input raw = lines raw
   let string_of_output output = output
 
-  let solve_part1 input =
-    most_common_letters input
-    |> Bytes.to_string
+  let solve_part1 input = most_common_letters input |> Bytes.to_string
 
   let solve_part2 input =
     least_common_letters input
