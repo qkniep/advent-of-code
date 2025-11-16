0235f3d9e2e8f9e9233db1289d523304f623864b
Quentin Kniep <hello@quentinkniep.com>
2025-11-16 19:21:38 +0100

    solve 2016 day 6

--- ocaml/main.ml
+++ ocaml/main.ml
@@ -50,6 +50,7 @@ let () =
     | 2016, 3 -> (module Year_2016.Day03.Day03 : DAY)
     | 2016, 4 -> (module Year_2016.Day04.Day04 : DAY)
     | 2016, 5 -> (module Year_2016.Day05.Day05 : DAY)
+    | 2016, 6 -> (module Year_2016.Day06.Day06 : DAY)
     | _ ->
         failwith (Printf.sprintf "Day %d of year %d not implemented\n" day year)
   in
