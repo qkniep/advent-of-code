94b36482f35db7ff4f55a94501e7eab9ad9b415d
Quentin Kniep <hello@quentinkniep.com>
2025-11-16 12:49:46 +0100

    rewrite AOCaml helper library for Core

--- ocaml/aocaml/input.ml
+++ ocaml/aocaml/input.ml
@@ -1,13 +1,12 @@
+open Core
+
 let load_input_file year day =
   let path = Printf.sprintf "../data/%d/inputs/day%02d.txt" year day in
-  let ic = open_in path in
-  let content = really_input_string ic (in_channel_length ic) in
-  close_in ic;
-  content
+  In_channel.read_all path
 
 let read_lines () =
   let rec loop acc =
-    match input_line stdin with
+    match In_channel.input_line_exn In_channel.stdin with
     | line -> loop (line :: acc)
     | exception End_of_file -> List.rev acc
   in
@@ -17,21 +16,21 @@ let read_all () =
   let buf = Buffer.create 1024 in
   (try
      while true do
-       Buffer.add_string buf (input_line stdin);
+       Buffer.add_string buf (In_channel.input_line_exn In_channel.stdin);
        Buffer.add_char buf '\n'
      done
    with End_of_file -> ());
   Buffer.contents buf
 
-let split s ~on = String.split_on_char on s
-let lines s = split ~on:'\n' s |> List.filter (fun x -> x <> "")
-let words s = s |> split ~on:' ' |> List.filter (fun x -> x <> "")
-let ints s = s |> words |> List.map int_of_string
-let comma_ints s = s |> split ~on:',' |> List.map int_of_string
+let split s ~on = String.split_on_chars ~on:[ on ] s
+let split_non_empty s ~on = split ~on s |> List.filter ~f:(Fn.non String.is_empty)
+let lines s = split_non_empty ~on:'\n' s
+let words s = split_non_empty ~on:' ' s
+let ints s = s |> words |> List.map ~f:int_of_string
+let comma_ints s = s |> split ~on:',' |> List.map ~f:int_of_string
 
 let digits_of_string s =
-  s |> String.to_seq
-  |> Seq.map (fun c -> Char.code c - Char.code '0')
-  |> List.of_seq
+  String.to_list s
+  |> List.map ~f:(fun c -> Char.to_int c - Char.to_int '0')
 
-let int_grid lines = List.map digits_of_string lines
+let int_grid lines = List.map ~f:digits_of_string lines
--- ocaml/aocaml/timing.ml
+++ ocaml/aocaml/timing.ml
@@ -1,22 +1,22 @@
+open Core
+
 let time f =
-  let t0 = Unix.gettimeofday () in
+  let t0 = Core_unix.gettimeofday () in
   let res = f () in
-  let t1 = Unix.gettimeofday () in
-  (* Printf.printf "Time: %.3f ms\n%!" (t1 -. t0); *)
+  let t1 = Core_unix.gettimeofday () in
   (res, t1 -. t0)
 
 let benchmark ~reps f =
-  let t0 = Unix.gettimeofday () in
+  let t0 = Core_unix.gettimeofday () in
   for _ = 1 to reps do
     ignore (f ())
   done;
-  let t1 = Unix.gettimeofday () in
-  (* Printf.printf "Average time: %.3f ms\n%!" ((t1 -. t0) /. float_of_int reps) *)
-  (t1 -. t0) /. float_of_int reps
+  let t1 = Core_unix.gettimeofday () in
+  (t1 -. t0) /. Int.to_float reps
 
 let time_to_str secs =
-  match secs with
-  | s when s < 1e-6 -> Printf.sprintf "%.1f ns" (s *. 1e9)
-  | s when s < 1e-3 -> Printf.sprintf "%.1f μs" (s *. 1e6)
-  | s when s < 1.0 -> Printf.sprintf "%.1f ms" (s *. 1e3)
-  | s -> Printf.sprintf "%.1f s" s
+  let open Float.O in
+  if secs < 1e-6 then Printf.sprintf "%.1f ns" (secs * 1e9)
+  else if secs < 1e-3 then Printf.sprintf "%.1f μs" (secs * 1e6)
+  else if secs < 1.0 then Printf.sprintf "%.1f ms" (secs * 1e3)
+  else Printf.sprintf "%.1f s" secs
