d7efc0447ab389bf3dc31cf5c9e6e8227d1328b9
Quentin Kniep <hello@quentinkniep.com>
2025-11-16 17:52:39 +0100

    support variants in tests

--- ocaml/aocaml/test.ml
+++ ocaml/aocaml/test.ml
@@ -3,28 +3,37 @@ open Input
 open Timing
 
 let test_day (module D : DAY) year day solutions =
+  Printf.printf "%d Day %d: %s\n%!" year day D.name;
+
+  let raw = load_input_file_tests year day in
+  let input = D.parse_input raw in
+
+  let test_variants part variants solution =
+    let num_variants = List.length variants in
+    List.iteri
+      (fun i (name, f) ->
+        let res, t = time (fun () -> f input) in
+        let out = D.string_of_output res in
+        assert (String.equal out solution);
+        let t = time_to_str t in
+        let () =
+          if name = "" then
+            Printf.printf " |> Part %d \x1b[0;32mPASSED\x1b[0m \x1b[90m%s\x1b[0m\t%s\n%!" part t out
+          else if i < num_variants - 1 then
+            Printf.printf "    ├ %s \x1b[0;32mPASSED\x1b[0m \x1b[90m%s\x1b[0m\t%s\n%!" name t out
+          else Printf.printf "    └ %s \x1b[0;32mPASSED\x1b[0m \x1b[90m%s\x1b[0m\t%s\n%!" name t out
+        in
+        ())
+      variants
+  in
+
   let part1_solution, part2_solution =
     match solutions with
     | [s1; s2] -> s1, s2
     | _ -> failwith "should have 2 solutions (part 1 and part 2)"
   in
 
-  Printf.printf "%d Day %d: %s\n%!" year day D.name;
-  
-  let t0 = Core_unix.gettimeofday () in
-  let raw = load_input_file_tests year day in
-  let input = D.parse_input raw in
-  let output = D.solve_part1 input in
-  let out = D.string_of_output output in
-  assert (String.equal out part1_solution);
-  let t1 = Core_unix.gettimeofday () in
-  let dt = time_to_str (t1 -. t0) in
-  Printf.printf " |> Part 1 \x1b[0;32mPASSED\x1b[0m \x1b[90m%s\x1b[0m\n%!" dt;
-  
-  let t0 = Core_unix.gettimeofday () in
-  let output = D.solve_part2 input in
-  let out = D.string_of_output output in
-  assert (String.equal out part2_solution);
-  let t1 = Core_unix.gettimeofday () in
-  let dt = time_to_str (t1 -. t0) in
-  Printf.printf " |> Part 2 \x1b[0;32mPASSED\x1b[0m \x1b[90m%s\x1b[0m\n%!" dt;
+  let variants = [ ("", D.solve_part1) ] @ D.solve_part1_variants in
+  test_variants 1 variants part1_solution;
+  let variants = [ ("", D.solve_part2) ] @ D.solve_part2_variants in
+  test_variants 2 variants part2_solution
