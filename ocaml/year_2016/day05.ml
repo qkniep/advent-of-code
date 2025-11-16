10d911a4631a48dc754a431b7715a1340b860b6c
Quentin Kniep <hello@quentinkniep.com>
2025-11-16 18:32:51 +0100

    solve 2016 day 5 part 2

--- ocaml/year_2016/day05.ml
+++ ocaml/year_2016/day05.ml
@@ -2,25 +2,23 @@ open Core
 open Aocaml.Day_intf
 
 let has_n_zeroes hex n =
-  String.sub hex ~pos:0 ~len:n
-  |> String.for_all ~f:(fun c -> Char.equal c '0')
+  String.sub hex ~pos:0 ~len:n |> String.for_all ~f:(fun c -> Char.equal c '0')
 
 let hash_counter_hex input counter =
-    let input = String.concat [ input; string_of_int counter ] in
-    Md5.to_hex (Md5.digest_string input)
+  let input = String.concat [ input; string_of_int counter ] in
+  Md5.to_hex (Md5.digest_string input)
 
-let next_key input start =
-  Sequence.unfold ~init:start ~f:(fun i -> Some (i, i+1))
+let next_relevant_hex input start =
+  Sequence.unfold ~init:start ~f:(fun i -> Some (i, i + 1))
   |> Sequence.find_map ~f:(fun i ->
-    let hex = hash_counter_hex input i in
-    if has_n_zeroes hex 5 then Some (String.get hex 5, i+1) else None)
+         let hex = hash_counter_hex input i in
+         if has_n_zeroes hex 5 then Some (hex, i + 1) else None)
   |> Option.value_exn
 
 let password_sequence input =
   Sequence.unfold ~init:0 ~f:(fun cnt ->
-    let ch, next_cnt = next_key input cnt in
-    Some (ch, next_cnt)
-  )
+      let hex, next_cnt = next_relevant_hex input cnt in
+      Some ((hex.[5], hex.[6]), next_cnt))
 
 module Day05 : DAY = struct
   let name = "How About a Nice Game of Chess?"
@@ -33,32 +31,33 @@ module Day05 : DAY = struct
 
   let solve_part1 input =
     Sequence.take (password_sequence input) 8
-    |> Sequence.to_list
-    |> String.of_char_list
+    |> Sequence.map ~f:fst |> Sequence.to_list |> String.of_char_list
 
-  let solve_part2 _input = ""
+  let solve_part2 input =
+    let buf = Buffer.create 8 in
+    for _ = 0 to 7 do
+      Buffer.add_char buf '#'
+    done;
+    let buf = Bytes.of_string (Buffer.contents buf) in
+    password_sequence input
+    |> Sequence.map ~f:(fun (p, c) ->
+           let pos = Char.to_int p - Char.to_int '0' in
+           (pos, c))
+    |> Sequence.filter ~f:(fun (pos, _) -> pos >= 0 && pos <= 7)
+    |> Sequence.iter_until
+         ~f:(fun (pos, c) ->
+           let char = Bytes.get buf pos in
+           if Char.equal char '#' then Bytes.set buf pos c else ();
+           if Bytes.contains buf '#' then Continue ()
+           else Stop (Bytes.to_string buf))
+         ~finish:(fun () -> Bytes.to_string buf)
 
   (* no variants, just empty lists *)
   let solve_part1_variants = []
   let solve_part2_variants = []
 end
 
-let%test_unit "hex" =
-  let input = "abc" in
-  let i = 5017308 in
-  let input = String.concat [ input; string_of_int i ] in
-  let hex = Md5.to_hex (Md5.digest_string input) in
-  Printf.printf "HEX TEST: %s\n" hex;
-  let (c, new_cnt) = next_key "abc" 0 in
-  assert (Char.equal c '1');
-  assert (new_cnt = 3231929);
-  let (c, new_cnt) = next_key "abc" 3231929 in
-  assert (Char.equal c '1');
-  assert (new_cnt = 3231929);
-  let (c, _new_cnt) = next_key "abc" 5017308 in
-  assert (Char.equal c '8')
-
 let%test_unit "2016 day 5" =
   let open Aocaml.Test in
-  let solutions = [ "173787"; "548" ] in
+  let solutions = [ "4543c154"; "1050cbbd" ] in
   test_day (module Day05 : DAY) 2016 5 solutions
