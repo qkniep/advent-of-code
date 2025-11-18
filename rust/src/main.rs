7684ea05a69e9a32ec1b657ed1dc5d3d1fb8f966
Quentin Kniep <hello@quentinkniep.com>
2025-11-18 22:44:41 +0100

    use `clap` for CLI arguments of runner

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,20 +1,49 @@
 mod years;
-use years::*;
+
+use clap::Parser;
+
+use self::years::*;
+
+#[derive(Parser)]
+#[clap(author, version, about)]
+struct Args {
+    /// Year to run
+    #[clap(short, long)]
+    year: u16,
+
+    /// Day to run
+    #[clap(short, long)]
+    day: u16,
+
+    /// Part of the day to run (1 or 2)
+    #[clap(short, long, default_value_t = 0)]
+    part: u8,
+
+    /// Input file override
+    #[clap(short, long)]
+    input: Option<String>,
+
+    /// Generate ASCII/graph visualization
+    #[clap(long)]
+    plot: bool,
+}
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let year = args.get(1).expect("year provided");
-    let year: u16 = year.parse().expect("year is number");
-    let day = args.get(2).expect("day provided");
-    let day: u16 = day.parse().expect("day is number");
-    let input = std::fs::read_to_string(format!("inputs/y{}/day{}.txt", year, day)).unwrap();
+    // parse CLI arguments
+    let args = Args::parse();
+    let Args { year, day, .. } = args;
+
+    // load correct input file
+    let path = format!("../data/{}/inputs/day{:02}.txt", year, day);
+    let input = std::fs::read_to_string(path).unwrap();
+
     match year {
         2024 => match day {
             1 => println!("Part 1: {}", y2024::day01::part1(&input)),
-            d if d <= 25 => unimplemented!(),
+            d if d <= 25 => todo!(),
             d => panic!("invalid day {}", d),
         },
-        y if (2015..2025).contains(&y) => unimplemented!(),
+        y if (2015..2025).contains(&y) => todo!(),
         y => panic!("invalid year {}", y),
     }
 }
