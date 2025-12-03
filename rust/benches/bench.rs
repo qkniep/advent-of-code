72c7e7b700437a09e924a5dea57e64bd460192e9
Quentin Kniep <hello@quentinkniep.com>
2025-12-03 22:03:26 +0100

    extend benchmark

--- rust/benches/bench.rs
+++ rust/benches/bench.rs
@@ -1,39 +1,56 @@
+use aoc::core::*;
+use aoc::years;
+
 fn main() {
     divan::main();
 }
 
-#[divan::bench_group]
+#[divan::bench_group(sample_size = 100, sample_count = 1000)]
 mod y2025 {
-    use aoc::core::*;
-    use aoc::years::*;
+    use super::*;
 
     #[divan::bench]
     fn day01(bencher: divan::Bencher) {
         let input = input::load_input(2025, 1);
-        let parsed = y2025::day01::parse(&input);
+        let parsed = years::y2025::day01::parse(&input);
 
         bencher.with_inputs(|| parsed.clone()).bench_values(|p| {
-            let _ = y2025::day01::part1(p);
+            let _ = years::y2025::day01::part1(p);
         });
     }
 
     #[divan::bench]
     fn day02(bencher: divan::Bencher) {
         let input = input::load_input(2025, 2);
-        let parsed = y2025::day02::parse(&input);
+        let parsed = years::y2025::day02::parse(&input);
 
         bencher.with_inputs(|| parsed.clone()).bench_values(|p| {
-            let _ = y2025::day02::part1_iter(p);
+            let _ = years::y2025::day02::part1_iter(p);
         });
     }
 
-    #[divan::bench]
-    fn day03(bencher: divan::Bencher) {
-        let input = input::load_input(2025, 3);
-        let parsed = y2025::day03::parse(&input);
+    #[divan::bench_group]
+    mod day03 {
+        use super::*;
 
-        bencher.with_inputs(|| parsed.clone()).bench_values(|p| {
-            let _ = y2025::day03::part1(p);
-        });
+        #[divan::bench]
+        fn part1(bencher: divan::Bencher) {
+            let input = input::load_input(2025, 3);
+            let parsed = years::y2025::day03::parse(&input);
+
+            bencher.with_inputs(|| parsed.clone()).bench_values(|p| {
+                let _ = years::y2025::day03::part1_custom_max(p);
+            });
+        }
+
+        #[divan::bench]
+        fn part2(bencher: divan::Bencher) {
+            let input = input::load_input(2025, 3);
+            let parsed = years::y2025::day03::parse(&input);
+
+            bencher.with_inputs(|| parsed.clone()).bench_values(|p| {
+                let _ = years::y2025::day03::part2_custom_max(p);
+            });
+        }
     }
 }
